66a04a3b535ab7f2d92a67bb8174e09850f0f84e
Neerajakshulu-Ambati <neerajakshuluambati1@gmail.com>
2025-04-07 13:27:19 +0530

    new rep1.sh

---
+++ rep1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir neeraj
+cd neeraj
+git init
+touch README.md
+git add README.md
+git commit -m "Initial commit"
+git remote add origin ssh://git@github.com/neerajakshuluambati/neeraj.git
